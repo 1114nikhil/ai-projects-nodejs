d6fe7ddb231c0fc5c4bbcb9ee11970db92b7fecc
1114nikhil <nikhiltk1114@gmail.com>
2025-08-03 22:03:34 +0530

    ts changes

---
+++ fundamentals/src/index.ts
@@ -0,0 +1,23 @@
+import {OpenAI} from 'openai';
+
+// Create an Instance of OpenAI class
+const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
+
+
+const main =async ()=>{
+    //  Define the prompt
+    const prompt = "Hi How are you?"
+const response = await openai.responses.create({
+    model: "gpt-4.1-nano-2025-04-14",
+    input: [
+        {
+            role:"user",
+            content:prompt
+        }
+    ]
+});
+
+console.log(response.output_text);
+}
+
+main();
