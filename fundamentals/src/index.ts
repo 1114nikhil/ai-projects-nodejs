652cdc1453805093c07839a4ab1423bea1c6e27c
1114nikhil <nikhiltk1114@gmail.com>
2025-08-05 21:34:04 +0530

    how to check no. of tokens

--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -1,4 +1,5 @@
 import {OpenAI} from 'openai';
+import { encoding_for_model } from 'tiktoken';
 
 // Create an Instance of OpenAI class
 const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
@@ -19,5 +20,11 @@ const response = await openai.responses.create({
 
 console.log(response.output_text);
 }
+const encodePrompt =(prompt: string) =>{
+    const encoder =encoding_for_model("gpt-4.1-nano-2025-04-14")
+    const tokens = encoder.encode(prompt)
+    console.log(tokens)
+}
 
+encodePrompt("Hi How are you?");
 main();
