eb3e9d30b9c2ad8c66377b42b50a4047422cc2b1
1114nikhil <nikhiltk1114@gmail.com>
2025-08-06 06:38:47 +0530

    Roles

--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -4,13 +4,49 @@ import { encoding_for_model } from 'tiktoken';
 // Create an Instance of OpenAI class
 const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
 
+// #region test open ai working
+// const main =async ()=>{
+//     //  Define the prompt
+//     const prompt = "Hi How are you?"
+// const response = await openai.responses.create({
+//     model: "gpt-4.1-nano-2025-04-14",
+//     input: [
+//         {
+//             role:"user",
+//             content:prompt
+//         }
+//     ]
+// });
 
+// console.log(response.output_text);
+// }
+// #endregion
+
+
+// #region check no. of tokens
+// const encodePrompt =(prompt: string) =>{
+//     const encoder =encoding_for_model("gpt-4.1-nano-2025-04-14")
+//     const tokens = encoder.encode(prompt)
+//     console.log(tokens)
+// }
+// encodePrompt("Hi How are you?");
+// #endregion
+
+// #region understanding Roles
+// Roles
+// 1. system : we tell ai how to response
+// 2. user : we send the query
+// 3. assistance : ai sends the response
 const main =async ()=>{
     //  Define the prompt
     const prompt = "Hi How are you?"
 const response = await openai.responses.create({
     model: "gpt-4.1-nano-2025-04-14",
     input: [
+        {
+            role:"system",
+            content:"You respond with greeting 'Namaste' in the beginning and you always respond in the JSON formate, like this :{'greeting':'greeting here','message':'reply here'} "
+        },
         {
             role:"user",
             content:prompt
@@ -20,11 +56,5 @@ const response = await openai.responses.create({
 
 console.log(response.output_text);
 }
-const encodePrompt =(prompt: string) =>{
-    const encoder =encoding_for_model("gpt-4.1-nano-2025-04-14")
-    const tokens = encoder.encode(prompt)
-    console.log(tokens)
-}
-
-encodePrompt("Hi How are you?");
+// #endregion
 main();
