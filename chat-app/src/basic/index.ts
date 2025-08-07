06e886a1adf484cb28e75c76df945aa6a6042c48
1114nikhil <nikhiltk1114@gmail.com>
2025-08-08 03:53:58 +0530

    Set the token limit

--- chat-app/src/basic/index.ts
+++ chat-app/src/basic/index.ts
@@ -1,14 +1,17 @@
 import {OpenAI} from 'openai';
+import { encoding_for_model } from 'tiktoken';
 
 // Create an Instance of OpenAI class
 const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
-
+const encoder= encoding_for_model("gpt-4.1-nano-2025-04-14")
+const MAX_TOKEN=500;
 const context :OpenAI.Chat.Completions.ChatCompletionMessageParam[]= [
 {
                 role:"system",
                 content:"Act like a cool bro"
             },
 ];
+
 
 const createChat= async() =>{
     const response=await openai.chat.completions.create({
@@ -20,6 +23,9 @@ const createChat= async() =>{
     context.push(msgResponse);
   } else {
     console.error("No message returned in response.");
+  }
+  if(response.usage && response.usage?.total_tokens>MAX_TOKEN){
+    removeOlderToken();
   }
     console.log(response.choices[0]?.message.content);
 }
@@ -34,6 +40,38 @@ process.stdin.addListener("data", async (input) => {
   await createChat();
 });
 
+const getContextLength=()=>{
+  let length=0;
+  context.forEach(
+    (message:OpenAI.Chat.Completions.ChatCompletionMessageParam)=>{
+      if(typeof message.content==="string"){
+        length +=encoder.encode(message.content).length;
+      }else if(Array.isArray(message.content)) {
+        message.content.forEach((content)=>{
+          if(content.type==="text"){
+            length +=encoder.encode(content.text).length;
+          }
+        });
+      }
+    }
+  );
+  return length;
+}
+
+const removeOlderToken =()=>{
+let contextLenght = getContextLength();
+while(contextLenght>MAX_TOKEN){
+  for(let i=0;i<context.length;i++){
+    const message= context[i];
+    if(message?.role!=="system"){
+      context.splice(i,1)
+      contextLenght=getContextLength();
+      console.log("Update context length :",contextLenght)
+      break;
+    }
+  }
+}
+}
 // #region Headfirst way
 // process.stdin.addListener("data",async (input)=>{
 //     const userInput =input.toString().trim();
